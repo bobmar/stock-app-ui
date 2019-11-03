d33929b7b4d852b0fd4ab6dbfbcc31ddcaf7b2d1
bobmar <bobmaraz@yahoo.com>
2019-11-03 09:42:12 -0700

    Remove useStyles

--- src/components/signals/SignalSection.js
+++ src/components/signals/SignalSection.js
@@ -7,28 +7,15 @@ import {makeStyles} from '@material-ui/core/styles'
 
 const SignalSection = function ( props) {
 
-    const useStyles = makeStyles({
-        root: {
-            width: '100%',
-        },
-        tableWrapper: {
-            maxHeight:600,
-            overflow:'auto',
-        }
-    })
-
-    const classes = useStyles();
     return (
-        <div className={classes.tableWrapper}>
-        <Table stickyHeader>
-            <SignalHeader/>
-            <TableBody>
-                {
-                    props.signalList.map((s)=><SignalRow  key={s.tickerSymbol} signal={s}/>)
-                }
-            </TableBody>
-        </Table>
-        </div>
+    <Table stickyHeader>
+        <SignalHeader/>
+        <TableBody>
+            {
+                props.signalList.map((s)=><SignalRow  key={s.tickerSymbol} signal={s}/>)
+            }
+        </TableBody>
+    </Table>
     )
 }
 
